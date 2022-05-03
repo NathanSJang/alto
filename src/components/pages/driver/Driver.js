b08a92d37b3c7917c0cc1c011c079a780a418f52
Nathan S Jang <nathan.s.jang@gmail.com>
2022-05-03 15:26:36 -0700

    add css for Driver page

--- src/components/pages/driver/Driver.js
+++ src/components/pages/driver/Driver.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PageLayout from '../../layouts/pageLayout/PageLayout';
+import Divider from '../../layouts/divider/Divider';
 
 export default function Driver(props) {
   const { data } = props;
@@ -9,15 +10,24 @@ export default function Driver(props) {
       disabled
       btnText='Contact Driver'
     >
-      <div>
+      <div className='driver-img-container'>
         <img
+          className='driver-img'
           src={data.image}
           alt='driver-photo'
         />
       </div>
-      Your Driver
-      {data.name}
-      {data.bio}
+      <div className='driver-disc-container'>
+        <div className='driver-title'>Your Driver</div>
+        <div className='driver-name'>
+          {data.name}
+        </div>
+        <div className='driver-bio'>
+          {/* <div className='divider'></div> */}
+          <Divider />
+          {data.bio}
+        </div>
+      </div>
     </PageLayout>
   )
 }
